0e7099209e37c0758570b45ff08b7f4aceac67c1
Kairo0628 <112845598+Kairo0628@users.noreply.github.com>
2025-10-02 12:13:42 +0900

    season

--- web/season/data_processor.py
+++ web/season/data_processor.py
@@ -8,18 +8,22 @@ TOP5_GENRE = os.path.join(settings.SEASON_DATAFRAME_DIR, 'every_season_top5_genr
 TOP10_COUNT = os.path.join(settings.SEASON_DATAFRAME_DIR, 'every_season_top10_count.csv')
 TOP10_RANK = os.path.join(settings.SEASON_DATAFRAME_DIR, 'every_season_top10_rank.csv')
 
-def top5_chart_in(season):
-    df = pd.read_csv(TOP10_COUNT)
+def get_main_chart(season):
+    df_top5 = pd.read_csv(TOP10_COUNT)
+    df_top5_rank = pd.read_csv(TOP10_RANK)
+
     if season == 'spring':
-        df = df[df['계절'] == '봄']
+        df_top5 = df_top5[df_top5['계절'] == '봄']
+        df_top5_rank = df_top5_rank[df_top5_rank['계절'] == '봄']
     elif season == 'summer':
-        df = df[df['계절'] == '여름']
+        df_top5 = df_top5[df_top5['계절'] == '여름']
+        df_top5_rank = df_top5_rank[df_top5_rank['계절'] == '여름']
     elif season == 'fall':
-        df = df[df['계절'] == '가을']
-    elif season == 'winter':
-        df = df[df['계절'] == '겨울']
+        df_top5 = df_top5[df_top5['계절'] == '가을']
+        df_top5_rank = df_top5_rank[df_top5_rank['계절'] == '가을']
+    else:
+        df_top5 = df_top5[df_top5['계절'] == '겨울']
+        df_top5_rank = df_top5_rank[df_top5_rank['계절'] == '겨울']
 
-    song_name = df['곡명'].tolist()[:5]
-    chart_in_count = df['count'].tolist()[:5]
-
-    return song_name, chart_in_count
+    top5_song_name = df_top5['곡명'].tolist()[:5]
+    top5_chart_in_count = df_top5['count'].tolist()[:5]
